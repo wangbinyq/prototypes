d500849a40743962268d1767e21a20a83c790dc3
wangb <wangbinyq@gmail.com>
2025-08-18 17:40:07 +0800

    feat(paddle-square): add bounce particle effects and lighting to ball

--- paddle-square/ball.gd
+++ paddle-square/ball.gd
@@ -6,6 +6,8 @@ extends MeshInstance3D
 @export_range(0, 100) var start_x_speed := 8.0
 @export_range(0, 100) var constant_y_speed := 10.0
 @export_range(0, 100) var extents := 0.5
+@export var bounce_particle: GPUParticles3D
+@export var bounce_particle_emission := 20
 
 var pos: Vector2
 var velocity: Vector2
@@ -20,12 +22,16 @@ func move(delta: float):
     pos += velocity * delta
 
 func bounce_x(boundary: float):
+    var duration = (pos.x - boundary) / (velocity.x)
     pos.x = 2 * boundary - pos.x
     velocity.x = - velocity.x
+    emit_bounce_particle(boundary, pos.y - velocity.y * duration, 90 if boundary < 0 else 270)
 
 func bounce_y(boundary: float):
+    var duration = (pos.y - boundary) / (velocity.y)
     pos.y = 2 * boundary - pos.y
     velocity.y = - velocity.y
+    emit_bounce_particle(pos.x - velocity.x * duration, boundary, 0 if boundary < 0 else 180)
 
 func start_new_game():
     pos = Vector2(0, 0)
@@ -41,3 +47,9 @@ func end_game():
 func set_x_position_and_speed(start: float, speed_factor: float, delta: float):
     velocity.x = speed_factor * max_x_speed
     pos.x = start + velocity.x * delta
+
+func emit_bounce_particle(x: float, y: float, r: float):
+    bounce_particle.position = Vector3(x, 0, -y)
+    bounce_particle.rotation = Vector3(0, 0, r)
+    bounce_particle.amount = bounce_particle_emission
+    bounce_particle.restart()
