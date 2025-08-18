10a181159884801524ee929da0ae0919a50e00eb
wangb <wangbinyq@gmail.com>
2025-08-18 18:13:07 +0800

    feat(paddle-square): add start particle effect and update ball initialization
    - Introduce start_particle property to ball.gd for game start visualization
    - Configure new particle system in PaddleSquare.tscn with emission parameters
    - Adjust ball start_new_game() method to emit start particles on game begin
    - Update scene load steps and particle material configurations

--- paddle-square/ball.gd
+++ paddle-square/ball.gd
@@ -8,48 +8,52 @@ extends MeshInstance3D
 @export_range(0, 100) var extents := 0.5
 @export var bounce_particle: GPUParticles3D
 @export var bounce_particle_emission := 20
+@export var start_particle: GPUParticles3D
+@export var start_particle_emission := 100
 
 var pos: Vector2
 var velocity: Vector2
 
 func _ready() -> void:
-    hide()
+	hide()
 
 func update_visualization():
-    position = Vector3(pos.x, 0, -pos.y)
+	position = Vector3(pos.x, 0, -pos.y)
 
 func move(delta: float):
-    pos += velocity * delta
+	pos += velocity * delta
 
 func bounce_x(boundary: float):
-    var duration = (pos.x - boundary) / (velocity.x)
-    pos.x = 2 * boundary - pos.x
-    velocity.x = - velocity.x
-    emit_bounce_particle(boundary, pos.y - velocity.y * duration, 90 if boundary < 0 else 270)
+	var duration = (pos.x - boundary) / (velocity.x)
+	pos.x = 2 * boundary - pos.x
+	velocity.x = - velocity.x
+	emit_bounce_particle(boundary, pos.y - velocity.y * duration, 90 if boundary < 0 else 270)
 
 func bounce_y(boundary: float):
-    var duration = (pos.y - boundary) / (velocity.y)
-    pos.y = 2 * boundary - pos.y
-    velocity.y = - velocity.y
-    emit_bounce_particle(pos.x - velocity.x * duration, boundary, 0 if boundary < 0 else 180)
+	var duration = (pos.y - boundary) / (velocity.y)
+	pos.y = 2 * boundary - pos.y
+	velocity.y = - velocity.y
+	emit_bounce_particle(pos.x - velocity.x * duration, boundary, 0 if boundary < 0 else 180)
 
 func start_new_game():
-    pos = Vector2(0, 0)
-    update_visualization()
-    velocity.x = randf_range(-max_start_x_speed, max_start_x_speed)
-    velocity.y = - constant_y_speed
-    show()
+	pos = Vector2(0, 0)
+	update_visualization()
+	velocity.x = randf_range(-max_start_x_speed, max_start_x_speed)
+	velocity.y = - constant_y_speed
+	show()
+	start_particle.amount = start_particle_emission
+	start_particle.restart()
 
 func end_game():
-    pos.x = 0
-    hide()
+	pos.x = 0
+	hide()
 
 func set_x_position_and_speed(start: float, speed_factor: float, delta: float):
-    velocity.x = speed_factor * max_x_speed
-    pos.x = start + velocity.x * delta
+	velocity.x = speed_factor * max_x_speed
+	pos.x = start + velocity.x * delta
 
 func emit_bounce_particle(x: float, y: float, r: float):
-    bounce_particle.position = Vector3(x, 0, -y)
-    bounce_particle.rotation = Vector3(0, 0, r)
-    bounce_particle.amount = bounce_particle_emission
-    bounce_particle.restart()
+	bounce_particle.position = Vector3(x, 0, -y)
+	bounce_particle.rotation = Vector3(0, 0, r)
+	bounce_particle.amount = bounce_particle_emission
+	bounce_particle.restart()
